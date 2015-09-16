ca6fab1ce30698b25f42a7ade8b29c6504578f0e
Moorecj <Moorecj@hotmail.co.nz>
2015-09-16 16:13:27 +1200

    Merge branch 'Kortaza'

--- DX10 Framework/Files/Gameplay/Gameplay/Arena/ArenaTile.cpp
+++ DX10 Framework/Files/Gameplay/Gameplay/Arena/ArenaTile.cpp
@@ -1,11 +1,57 @@
-#include "ArenaTile.h"
+/*
+* Bachelor of Software Engineering
+* Media Design School
+* Auckland
+* New Zealand
+*
+* (c) 2005 - 2015 Media Design School
+*
+* File Name : ArenaTile.cpp
+* Description : Tile to hold information about a small section of the Arena Floor
+* Author :	Callan Moore
+* Mail :	Callan.Moore@mediadesign.school.nz
+*/
 
+// Inclusion Guards
+#include "ArenaTile.h"
 
 ArenaTile::ArenaTile()
 {
 }
 
-
 ArenaTile::~ArenaTile()
 {
+}
+
+bool ArenaTile::Initialise(DX10_Renderer* _pDX10_Renderer, DX10_Mesh_Generic* _pMesh, DX10_Shader_LitTex* _pShader, UINT _texID)
+{
+	if (_pDX10_Renderer == 0 || _pMesh == 0 || _pShader == 0)
+	{
+		// Initialization is invalid if any passed pointers are NULL
+		return false;
+	}
+	BaseInitialise();
+	m_pRenderer = _pDX10_Renderer;
+	m_pMesh = _pMesh;
+	m_pShader_LitTex = _pShader;
+	m_texID = _texID;
+	
+	return true;
+}
+
+void ArenaTile::Process(float _dt)
+{
+	// TO DO CAL - Determine the correct image to be displayed
+}
+
+void ArenaTile::Render()
+{
+	// Put the Object Specific variables into a structure
+	TLitTex _litTex;
+	_litTex.pMesh = m_pMesh;
+	_litTex.pMatWorld = &m_matWorld;
+	_litTex.textureID = m_texID;
+
+	// Set the Shader to Render the Tile
+	m_pShader_LitTex->Render(_litTex, TECH_LITTEX_STANDARD);
 }
--- DX10 Framework/Files/Gameplay/Gameplay/Arena/ArenaTile.h
+++ DX10 Framework/Files/Gameplay/Gameplay/Arena/ArenaTile.h
@@ -1,8 +1,51 @@
+/*
+* Bachelor of Software Engineering
+* Media Design School
+* Auckland
+* New Zealand
+*
+* (c) 2005 - 2015 Media Design School
+*
+* File Name : ArenaTile.h
+* Description : Tile to hold information about a small section of the Arena Floor
+* Author :	Callan Moore
+* Mail :	Callan.Moore@mediadesign.school.nz
+*/
+
+// Inclusion Guards
 #pragma once
-class ArenaTile
+#ifndef __ARENATILE_H__
+#define __ARENATILE_H__
+
+// Local Includes
+#include "../../../DX10/DX10/3D Objects/DX10_Obj_Generic.h"
+#include "../../../DX10/DX10/Meshes/DX10_Mesh_Generic.h"
+#include "../../../DX10/DX10/Shaders/DX10_Shader_LitTex.h"
+
+class ArenaTile :
+	public DX10_Obj_Generic
 {
 public:
-	ArenaTile();
-	~ArenaTile();
-};
 
+	/***********************
+	* ArenaTile: Default Constructor for Arena Tile class
+	* @author: Callan Moore
+	********************/
+	ArenaTile();
+
+	/***********************
+	* ~ArenaTile: Default Destructor for Arena Tile class
+	* @author: Callan Moore
+	********************/
+	~ArenaTile();
+
+	// TO DO
+	bool Initialise(DX10_Renderer* _pDX10_Renderer, DX10_Mesh_Generic* _pMesh, DX10_Shader_LitTex* _pShader, UINT _texID);
+	void Process(float _dt);
+	void Render();
+
+private:
+	DX10_Shader_LitTex* m_pShader_LitTex;
+	UINT m_texID;
+};
+#endif	// __ARENATILE_H__
