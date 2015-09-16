6f2a6c60a083dab3bb7eeddc4a0834d653f276ec
Jc <jc.b.fowles@gmail.com>
2015-09-16 16:12:27 +1200

    Added the Player Class and some movement - needs testing

---
+++ DX10 Framework/Files/Gameplay/Gameplay/Orbs/Orb.cpp
@@ -0,0 +1,64 @@
+//
+// Bachelor of Software Engineering
+// Media Design School 
+// Auckland
+// New Zealand 
+// 
+// (c) 2005 - 2015 Media Design School 
+// 
+// File Name : Orb.h
+// Description : Class to handle all funtionality of the Player Controlled Orbs
+// Author : Jc Fowles 
+// Mail : Jc.Fowles@mediadesign.school.nz 
+//
+
+// This Include
+#include "Orb.h"
+
+
+Orb::Orb()
+{
+	m_acceleration = {0.0f,0.0f,0.0f};
+	m_velocity = { 0.0f, 0.0f, 0.0f };
+	m_density = 0.0f;
+	m_maxSpeed = 0.0f;
+	m_isAlive = true;
+}
+
+Orb::~Orb()
+{
+}
+
+bool Orb::Initialise(DX10_Renderer* _pRenderer, DX10_Mesh_Generic* _pMesh, DX10_Shader_LitTex* _pShader, UINT _textureID, float _density, float _maxSpeed)
+{	
+	// Initialise the object this is derived from
+	VALIDATE(DX10_Obj_LitTex::Initialise(_pRenderer, _pMesh, _pShader, _textureID));
+
+	// Check the passed in parameters
+	if ((_density < 0.0f) || (_maxSpeed < 0.0f))
+	{
+		return false;
+	}
+
+	// Store the initial state of the variables
+	m_density = _density;
+	m_maxSpeed = _maxSpeed;
+	m_isAlive = true;
+	   
+	// Succesful Initialization
+	return true;
+}
+  
+void Orb::Process(float _dt)
+{
+	// TO DO JC: Multiply by Delta Tick
+	m_velocity += m_acceleration;
+	m_velocity.Limit(m_maxSpeed);
+	m_pos += m_velocity;
+
+	// TO DO JC: This will change based on the friction of the tiles
+	m_acceleration *= 0.0f;
+
+	DX10_Obj_LitTex::Process(_dt);
+}
+
---
+++ DX10 Framework/Files/Gameplay/Gameplay/Orbs/Orb.h
@@ -0,0 +1,83 @@
+//
+// Bachelor of Software Engineering
+// Media Design School 
+// Auckland
+// New Zealand 
+// 
+// (c) 2005 - 2015 Media Design School 
+// 
+// File Name : Orb.h
+// Description : Class to handle all funtionality of the Player Controlled Orbs
+// Author : Jc Fowles 
+// Mail : Jc.Fowles@mediadesign.school.nz 
+//
+#pragma once
+
+#ifndef __ORB_H__
+#define __ORB_H__
+
+// Library Include
+
+// Local Includes
+#include "../../../DX10/DX10/3D Objects/DX10_Obj_Generic.h"
+#include "../../../DX10/DX10/3D Objects/DX10_Obj_LitTex.h"
+
+
+
+class Orb :
+	public DX10_Obj_LitTex
+{
+	// Member Functions
+public:
+
+	/***********************
+	* Orb: Default Constructor for the Player Controlled Orbs
+	* @author: Jc Fowles
+	* @return:
+	********************/
+	Orb();
+
+	/***********************
+	* Orb: Default Destructor for the Player Controlled Orbs
+	* @author: Jc Fowles
+	* @return:
+	********************/
+	~Orb();
+
+	/***********************
+	* Initialise: Initialise the Player Controlled Orbs
+	* @author: Jc Fowles
+	* @parameter: _pRenderer: Renderer for the Orb
+	* @parameter: _pMesh: Mesh for the Orb
+	* @parameter: _pShader: The Shader for the Orb
+	* @parameter: _textureID: ID of the single texture for the Orb to display
+	* @parameter: _density: The Density of the Orb to calculate its mass
+	* @parameter: _maxSpeed: The mass speed the Orb is allowed to travel at
+	* @return: bool : Successful initialization
+	********************/
+	bool Initialise(DX10_Renderer* _pRenderer, DX10_Mesh_Generic* _pMesh, DX10_Shader_LitTex* _pShader, UINT _textureID, float _density, float _maxSpeed);
+
+
+	// TO DO JC:
+	void Process(float _dt);
+
+	void SetAcceleration(v3float _acceleration){ m_acceleration = _acceleration; };
+
+protected:
+private:
+	// Member Variables
+public:
+protected:
+private:
+
+	v3float m_acceleration;
+	v3float m_velocity;
+	float m_maxSpeed;
+	float m_density;
+	bool m_isAlive;
+
+
+	
+};
+
+#endif;		// __ORB_H__
